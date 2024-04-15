8e5710acf3df9fd37d33cc1b127cf72541d23224
farpetrad <farpetrad@gmail.com>
2024-04-15 13:46:34 -0600

    update app

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,20 @@
 <template>
   <div id="wrap">
     <div class="main">
-      <nav-bar>
+      <nav-bar alignment="right">
+        <template #brand> Neal Cynkus </template>
         <template #content>
-          <nav-item-text
-            v-for="(item, index) in navItems"
-            :key="index"
-            :link="item"
-          >
-          </nav-item-text>
+          <template v-for="(item, index) in navItems">
+            <nav-item-text
+              v-if="!item.icon"
+              :key="index"
+              :link="item"
+            ></nav-item-text>
+            <nav-item-text v-if="item.icon" :key="index" :link="item">
+              <font-awesome-icon :icon="item.icon" size="2x" />
+            </nav-item-text>
+          </template>
+          <nav-item-image :icon="faLinkedin"></nav-item-image>
         </template>
       </nav-bar>
       <router-view></router-view>
@@ -17,24 +23,25 @@
 </template>
 
 <script setup lang="ts">
+import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
 import NavItemText from "./components/Navigation/NavItemText.vue";
 import NavItemTextProps from "@/types/NavItemProps";
 const navItems: NavItemTextProps[] = [
-  { href: "/", label: "Home" },
+  { href: "/skills", label: "Skills" },
+  { href: "/projects", label: "Projects" },
   { href: "/about", label: "About" },
-  {
-    href: "https://github.com/farpetrad/Portfolio",
-    label: "Github",
-    target: "_blank",
-  },
+  { href: "/contact", label: "Contact" },
   {
     href: "https://www.linkedin.com/in/neal-c-2749305b/",
     label: "LinkedIn",
     target: "_blank",
+    icon: faLinkedin,
+  },
+  {
+    href: "https://github.com/farpetrad/Portfolio",
+    label: "Github",
+    target: "_blank",
+    icon: faGithub,
   },
 ];
 </script>
-
-<style lang="scss">
-@import "scss/site.scss";
-</style>
--- src/types/NavItemProps.ts
+++ src/types/NavItemProps.ts
@@ -1,7 +1,10 @@
+import { IconDefinition } from "@fortawesome/free-brands-svg-icons";
+
 type NavItemProps = {
   label: string;
   href: string;
   target?: string;
+  icon?: IconDefinition | null;
 };
 
 export default NavItemProps;
