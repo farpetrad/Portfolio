3d47f87d07fdb10127a8f15945cbf7e65c7c8f73
farpetrad <farpetrad@gmail.com>
2024-04-22 14:53:21 -0600

    remove routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,33 +7,6 @@ const routes: Array<RouteRecordRaw> = [
     name: "home",
     component: HomeView,
   },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
-  },
-  {
-    path: "/projects",
-    name: "projects",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "projects" */ "../views/ProjectsView.vue"),
-  },
-  {
-    path: "/contact",
-    name: "contact",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/ContactView.vue"),
-  },
 ];
 
 const router = createRouter({
